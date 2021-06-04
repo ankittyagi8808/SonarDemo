85f6d01aa5c1bd80303159e0dcd4f39461ffe276
ankittyagi8808@gmail.com <ankitttyagi8808@gmail.com>
2021-06-04 13:22:58 +0530

    DemoCommit1

--- src/main/java/com/qrux/discussion/domain/Demo.java
+++ src/main/java/com/qrux/discussion/domain/Demo.java
@@ -27,4 +27,30 @@ public class Demo {
 		System.out.println("Test4");
 		System.out.println("Test4");
 	}
+	
+	public void test1()
+	{
+		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+		System.out.println("Test4");
+	}
 }
