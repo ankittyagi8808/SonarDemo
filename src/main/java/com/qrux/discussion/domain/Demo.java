afc4cf54a745635c9a61e850117081ad91ea4f0b
ankittyagi8808@gmail.com <ankitttyagi8808@gmail.com>
2021-06-04 09:30:24 +0530

    Removing an Comment 2

--- src/main/java/com/qrux/discussion/domain/Demo.java
+++ src/main/java/com/qrux/discussion/domain/Demo.java
@@ -5,10 +5,6 @@ public class Demo {
 	public void test()
 	{
 		System.out.println("Test");
-		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test4");
-		System.out.println("Test4");
-		System.out.println("Test4");
+		
 	}
 }
